2f89403bd15b69d399bee219c0010fb6637e589e
michal0604 <mn116@walla.co.il>
2019-07-23 20:31:28 +0300

    blA

---
+++ springPro/src/main/java/example1/ApplicationExample1.java
@@ -0,0 +1,19 @@
+package example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({ "example1" })
+public class ApplicationExample1 {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(ApplicationExample1.class, args);
+		
+		
+		
+	}
+}
